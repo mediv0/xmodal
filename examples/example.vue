ada9a51bc36e7133b58918f68151ff1d2d31b4dd
Mahdi Fakhr <36822136+MahdiFakhr@users.noreply.github.com>
2020-04-19 10:02:53 +0430

    :wheelchair: add example.vue for better testing environment

---
+++ examples/example.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <button @click="isModalOpen = !isModalOpen">open</button>
+        <xmodal v-model="isModalOpen" :params="options" />
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+import xmodal from "../src/entry";
+
+Vue.use(xmodal);
+
+export default {
+    data() {
+        return {
+            isModalOpen: false,
+            options: {
+                component: require("./modal.vue").default,
+                backgroundColor: "#000000",
+                opacity: "0.7",
+                mounted: this.modalOpen,
+                destroyed: this.modalClose
+            }
+        };
+    },
+    methods: {
+        modalOpen() {
+            console.log("modal is mounted");
+        },
+        modalClose() {
+            console.log("modals closed");
+        }
+    }
+};
+</script>
